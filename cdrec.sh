55068a1cdef756746bba7cc9324b3a6392c85582
Luan Belem Rivello <58953615+luanrivello@users.noreply.github.com>
2022-08-16 16:43:34 -0300

    Update cdrec.sh

--- cdrec.sh
+++ cdrec.sh
@@ -6,8 +6,12 @@ cdrec() {
     listall=$(ls -1A)
     firstitem=$(echo $listall | sed -n 1p)
 
-    # if there is only 1 item and its a directory then cd and check again
-    if [[ $(echo $listall | wc -l) == 1 && $(file -b $firstitem) == "directory" ]] \
-    cdrec $firstitem
-    
+    # if there is nothing inside or if you are trying to go back the directories then dont run
+    if [[ -z $firstitem ]] || [[ $(echo "$@" | cut -c1-2) == ".." ]]; then
+        return
+    # else if there is only 1 item and its a directory then cd and run again
+    elif [[ $(echo $listall | wc -l) == 1 && $(file -b $firstitem) == "directory" ]]; then
+        cdrec $firstitem
+    fi
+
 }
